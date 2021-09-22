e7b3bf8b6db3cde524e52d089f95ba6fb03d0c30
rmedx <65210483+rmedx@users.noreply.github.com>
2021-09-22 15:19:16 -0400

    edit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,36 @@
 const chai = require("chai");
 const chaiHttp = require('chai-http');
+const { test } = require("mocha");
 const assert = chai.assert;
 const server = require('../server');
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+    // Solve a puzzle with valid puzzle string: POST request to /api/solve
+    test('Solve a puzzle with valid puzzle string: POST request to /api/solve', function(done) {
+        chai.request(server)
+            .post('/api/solve')
+            .type('form')
+            .send({puzzle: "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."})
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.solution, '135762984946381257728459613694517832812936745357824196473298561581673429269145378');
+                done();
+            });
+    });
+    // Solve a puzzle with missing puzzle string: POST request to /api/solve
+    // Solve a puzzle with invalid characters: POST request to /api/solve
+    // Solve a puzzle with incorrect length: POST request to /api/solve
+    // Solve a puzzle that cannot be solved: POST request to /api/solve
+    // Check a puzzle placement with all fields: POST request to /api/check
+    // Check a puzzle placement with single placement conflict: POST request to /api/check
+    // Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+    // Check a puzzle placement with all placement conflicts: POST request to /api/check
+    // Check a puzzle placement with missing required fields: POST request to /api/check
+    // Check a puzzle placement with invalid characters: POST request to /api/check
+    // Check a puzzle placement with incorrect length: POST request to /api/check
+    // Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+    // Check a puzzle placement with invalid placement value: POST request to /api/check
 });
 
